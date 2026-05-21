00d28c07ef960f1ce02ac244cb6bc8984f39e908
Icey <sainiabhimanyu155@gmail.com>
2026-05-21 23:50:58 +0530

    something

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const cloudinary = require('cloudinary').v2;
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // Cloudinary configuration
